93a4587e841094bc73858c668816ae3cbef2c615
YKhemych <yosyp0108khemych@gmail.com>
2019-11-09 16:40:26 +0200

    Init users module, controller, service and connected to DB

---
+++ src/users/user.entity.ts
@@ -0,0 +1,21 @@
+import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
+
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ length: 25 })
+  firstName: string;
+
+  @Column({ length: 25 })
+  lastName: string;
+
+}
