9a0f2660ac1ee61c7a0097fcca60cd02df432da2
YKhemych <yosyp0108khemych@gmail.com>
2019-11-10 23:08:02 +0200

    Fixed mailer config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,15 @@ import { typeOrmConfig } from './config/typeorm.config';
 import { Connection } from 'typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { HandlebarsAdapter, MailerModule } from '@nest-modules/mailer';
+import { MailerModule } from '@nest-modules/mailer';
 import { mailerConfig } from './config/mailer.config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig),
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    MailerModule.forRoot(mailerConfig.mailerConfig),
     UsersModule,
     AuthModule,
-    MailerModule.forRoot({
-      transport: `smtps://${mailerConfig.email}:${mailerConfig.password}@smtp.gmail.com`,
-      template: {
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
-        },
-      },
-    }),
   ],
 })
 export class AppModule {
--- src/config/mailer.config.ts
+++ src/config/mailer.config.ts
@@ -1,5 +1,15 @@
+import { HandlebarsAdapter } from '@nest-modules/mailer';
 
 export const mailerConfig = {
   email: 'email@gmail.com',
   password: 'password',
+  mailerConfig: {
+    transport: `smtps://${this.email}:${this.password}@smtp.gmail.com`,
+    template: {
+      adapter: new HandlebarsAdapter(),
+      options: {
+        strict: true,
+      },
+    },
+  },
 };
